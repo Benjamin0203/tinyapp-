d42a34e8e4a71ccba47e0ae57859b783e72fb76b
Benjamin Meng <benjaminmeng1994@gmail.com>
2022-11-01 10:29:05 -0700

    delete added

--- express_server.js
+++ express_server.js
@@ -58,8 +58,6 @@ app.post("/urls", (req, res) => {
   
 });
 
-
-
 app.get("/u/:id", (req, res) => {
   // if (!urlDatabase[req.params.shortURL]) {
   //   res.status(404);
@@ -69,11 +67,17 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
+
+
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]}
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars)
 });
 
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id];
+  res.redirect(`/urls/`)
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
