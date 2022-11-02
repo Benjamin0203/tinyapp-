045ebdab4ddb0099e2bffaecf88f9c3c980f4fd9
Benjamin Meng <benjaminmeng1994@gmail.com>
2022-11-02 14:57:03 -0700

    reroute to login page

--- express_server.js
+++ express_server.js
@@ -99,7 +99,7 @@ app.post("/login", (req, res) => {
 //logout
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
-  res.redirect("/urls");
+  res.redirect("/login");
 });
 
 //Register
